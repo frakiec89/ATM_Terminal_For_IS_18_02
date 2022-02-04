74d63f7fabc5e7d0018754083500698fdf5c4e6f
frakiec89 <45938852+frakiec89@users.noreply.github.com>
2022-02-04 11:20:55 +0400

    Добавьте файлы проекта.

---
+++ ATM_Terminal_For_IS_18_02/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ATM_Terminal_For_IS_18_02
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btGo_Click(object sender, RoutedEventArgs e)
+        {
+            var content = File.ReadAllText("Content.json");
+            var us = JsonConvert.DeserializeObject<List<dynamic>>(content);
+
+            bool f = false;
+
+            foreach (var item in us)
+            {
+
+                var n = item.NumberCard;
+                var p = item.Pin;
+
+                if (n== tbNumber.Text &&  p == tbPin.Text)
+                {
+                    MessageBox.Show($"привет {item.UserName}");
+                    f = true;
+                    WindowsATM windowsATM = new WindowsATM(item);
+                    windowsATM.Show();
+                    this.Close();
+                    return; 
+                }
+            }
+            if(f == false)
+            {
+                MessageBox.Show($"Пользователь не  найден");
+            }
+        }
+    }
+}
---
+++ ATM_Terminal_For_IS_18_02/WindowsATM.xaml.cs
@@ -0,0 +1,107 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+
+namespace ATM_Terminal_For_IS_18_02
+{
+    /// <summary>
+    /// Логика взаимодействия для WindowsATM.xaml
+    /// </summary>
+    public partial class WindowsATM : Window
+    {
+
+        dynamic userOne; 
+
+        public WindowsATM(dynamic user)
+        {
+            InitializeComponent();
+            userOne = user;
+            tbName.Text = userOne.UserName;
+        }
+
+        private void btExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btOutSumma_Click(object sender, RoutedEventArgs e)
+        {
+            var content = File.ReadAllText("Content.json");
+            var us = JsonConvert.DeserializeObject<List<dynamic>>(content);
+
+            foreach (var item in us)
+            {
+                var n = item.NumberCard;
+                var p = item.Pin;
+                var b = item.Balance;
+
+                if (n == userOne.NumberCard && p == userOne.Pin)
+                {
+                    var s = b - Convert.ToDouble(tbOutSumma.Text);
+                    if ( s >=0)
+                    {
+                        item.Balance -= Convert.ToDouble(tbOutSumma.Text);
+                        MessageBox.Show("Заберите деньги");
+                        break;
+                    }
+                   else
+                    {
+                        MessageBox.Show("НЕ хватает  денег  на счету");
+                        return;
+                    }
+                }
+            }
+
+            content = JsonConvert.SerializeObject(us);
+
+            File.WriteAllText("Content.json", content);
+        }
+
+        private void btInSumma_Click(object sender, RoutedEventArgs e)
+        {
+            var content = File.ReadAllText("Content.json");
+            var us = JsonConvert.DeserializeObject<List<dynamic>>(content);
+
+            foreach (var item in us)
+            {
+
+                var n = item.NumberCard;
+                var p = item.Pin;
+                var b = item.Balance;
+
+                if (n == userOne.NumberCard && p == userOne.Pin)
+                {
+                    item.Balance += Convert.ToDouble(tbInSumma.Text);
+                    MessageBox.Show("деньги положены на счет");
+                    break;
+                }
+            }
+
+            content = JsonConvert.SerializeObject(us);
+
+            File.WriteAllText("Content.json", content);
+        }
+
+        private void btBalance_Click(object sender, RoutedEventArgs e)
+        {
+            var content = File.ReadAllText("Content.json");
+            var us = JsonConvert.DeserializeObject<List<dynamic>>(content);
+
+            foreach (var item in us)
+            {
+
+                var n = item.NumberCard;
+                var p = item.Pin;
+                var b = item.Balance;
+
+                if (n == userOne.NumberCard && p == userOne.Pin)
+                {
+                    tbBalance.Text = item.Balance;
+                    break;
+                }
+            }
+        }
+    }
+}
